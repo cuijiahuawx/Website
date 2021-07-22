09c8ad53c13c98fd91657ff029e4227b5a31f5de
cuijiahuawx <36182049+cuijiahuawx@users.noreply.github.com>
2021-07-22 14:33:55 +0000

    Deploying to gh-pages from  @ 3f67838e2fc679e48cae726ed5a6bc2f979b7079 🚀

---
+++ libs/backtotop.js
@@ -0,0 +1,30 @@
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener('scroll', function(){
+    var scroll = this.document.querySelector("#back-to-top-btn");
+    scroll.classList.toggle("active", window.scrollY > 500)
+})
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
